a08382306a71aa462cd6367a0ee49b555d417a5c
Bruno Trova <bltrova@gmail.com>
2025-01-17 10:26:16 -0800

    dom-modifying-lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById('main').remove();
+document.getElementsByTagName('newHeader');
+const newHeader = document.createElement('h1');
+newHeader.id ='victory';    
+newHeader.innerHTML = "Bruno is the champion";
